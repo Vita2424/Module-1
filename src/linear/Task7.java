815e70476d6c3b4909699ee5928e870545e91bd0
Violetta <vita2424@mail.ru>
2020-10-15 18:43:24 +0300

    Add Task7

--- src/linear/Task7.java
+++ src/linear/Task7.java
@@ -1,4 +1,21 @@
 package linear;
 
+import java.util.Scanner;
+
 public class Task7 {
+    public static void main(String[] args) {
+        System.out.println("Введите 2 числа для min(a,b):");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("Введите 2 числа для min(c,d):");
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        final int min = Math.min(a, b);
+        final int min2 = Math.min(c, d);
+
+        int otvet = Math.max(min, min2);
+        System.out.println("Решение max{min(a,b),min(c,d)}= " + otvet);
+    }
 }
